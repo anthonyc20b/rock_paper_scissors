17532d53770815673ba9ef14117a94b1ccc71922
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-02-07 14:33:21 -0700

    Adjusted compute logic for intended outcome
    Data was not resulting in a proper final result when comparing the
    human and computer choices. Had to re-write to properly store values
    in their variables to compare logic.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,33 @@
-let randomNumber = prompt("Enter test number:"); // - Used to manually test number ranges when 
+//let randomNumber = prompt("Enter test number:"); // - Used to manually test number ranges when 
 // included with commenting out the below random number logic generator.
 
-//let randomNumber = Math.floor((Math.random()* 100)+ 2);
+let randomNumber = Math.floor((Math.random()* 100)+ 2);
+let humanScore = 0;
+let computerScore = 0;
 console.log(randomNumber);
 
 function getComputerChoice() {
+    let computerChoice = "";
     if (randomNumber <= 33) {
-        console.log("Rock");
+        computerChoice = "Rock";
     } else if (randomNumber >= 34 && randomNumber <= 67) {
-        console.log("Paper");
+        computerChoice = "Paper";
     } else if (randomNumber >= 68 && randomNumber <= 101) {
-        console.log("Scissors");
+        computerChoice = "Scissors";
     } else {
         console.log("That is not a valid number!");
     }
+    console.log (computerChoice);
+    return computerChoice;
 }
 
-getComputerChoice()
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper, or Scissors?");
+    console.log(humanChoice);
+    return humanChoice;
+}
+
+
+function playRound (humanChoice, computerChoice){
+    
+}
