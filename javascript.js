4109cf8c7d802013013563be619c8f5cbfedfa0f
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-02-07 13:50:57 -0700

    Change logic for equal chances on each randomize
    By adding the +2 to the end of the Math.random() it allows for there to
    be 102 possible random numbers. Allowing then for an equal 33 potential
    numbers to trigger each computer decision event.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
-// let randomNumber = prompt("Enter test number:"); - Used to manually test number ranges when 
+let randomNumber = prompt("Enter test number:"); // - Used to manually test number ranges when 
 // included with commenting out the below random number logic generator.
- 
-let randomNumber = Math.floor(Math.random()* 100);
+
+//let randomNumber = Math.floor((Math.random()* 100)+ 2);
 console.log(randomNumber);
 
 function getComputerChoice() {
     if (randomNumber <= 33) {
         console.log("Rock");
-    } else if (randomNumber >= 34 && randomNumber <= 66) {
+    } else if (randomNumber >= 34 && randomNumber <= 67) {
         console.log("Paper");
-    } else if (randomNumber >= 67 && randomNumber <= 99) {
+    } else if (randomNumber >= 68 && randomNumber <= 101) {
         console.log("Scissors");
     } else {
         console.log("That is not a valid number!");
